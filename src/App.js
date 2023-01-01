aa1a040794b5c80d823000d5c28b87829127120f
hp <salah.dev@hotmail.com>
2023-01-01 16:24:17 +0100

    fixed some issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
+import Spinner from "./components/spinner/spinner-comp";
 
 const Header = React.lazy(() => import("./routes/header/header-comp"));
 const Category = React.lazy(() =>
@@ -13,7 +14,7 @@ const Cart = React.lazy(() => import("./routes/cart/cart-container"));
 export class App extends Component {
   render() {
     return (
-      <>
+      <Suspense fallback={<Spinner />}>
         <Header />
         <Switch>
           <Route exact path="/cart" component={Cart} />
@@ -28,7 +29,7 @@ export class App extends Component {
             render={(props) => <ProductPage {...props} />}
           />
         </Switch>
-      </>
+      </Suspense>
     );
   }
 }

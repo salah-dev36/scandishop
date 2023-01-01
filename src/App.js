a1d649a03530b4c7ada262c76bf141fe87e4e4ed
hp <salah.dev@hotmail.com>
2023-01-01 16:29:50 +0100

    fixed header import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 import Spinner from "./components/spinner/spinner-comp";
 
-const Header = React.lazy(() => import("./routes/header/header-comp"));
+import Header from "./routes/header/header-comp";
 const Category = React.lazy(() =>
   import("./routes/category/category-container")
 );
@@ -14,22 +14,24 @@ const Cart = React.lazy(() => import("./routes/cart/cart-container"));
 export class App extends Component {
   render() {
     return (
-      <Suspense fallback={<Spinner />}>
+      <>
         <Header />
-        <Switch>
-          <Route exact path="/cart" component={Cart} />
-          <Route
-            exact
-            path="/:category"
-            render={(props) => <Category {...props} />}
-          />
-          <Route
-            exact
-            path="/:category/:productId"
-            render={(props) => <ProductPage {...props} />}
-          />
-        </Switch>
-      </Suspense>
+        <Suspense fallback={<Spinner />}>
+          <Switch>
+            <Route exact path="/cart" component={Cart} />
+            <Route
+              exact
+              path="/:category"
+              render={(props) => <Category {...props} />}
+            />
+            <Route
+              exact
+              path="/:category/:productId"
+              render={(props) => <ProductPage {...props} />}
+            />
+          </Switch>
+        </Suspense>
+      </>
     );
   }
 }
